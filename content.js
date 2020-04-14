d6da94853c6e97a12fd2c18fbe11d7675fcad882
Estherpyr <60486583+Estherpyr@users.noreply.github.com>
2020-04-14 15:31:34 -0400

    Add files via upload

---
+++ content.js
@@ -0,0 +1,6 @@
+console.log('chrome extension runrunrun')
+
+var elts = document.getElementsByTagName('p');
+    for (var i = 0; i < elts.length; i++) {
+      elts[i].style['background-color'] = 'purple';
+    }
