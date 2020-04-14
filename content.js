a55cadd67b7cd310885f36203d281c425606a8e0
Estherpyr <60486583+Estherpyr@users.noreply.github.com>
2020-04-14 15:17:51 -0400

    Delete content.js

--- content.js
+++
@@ -1,6 +0,0 @@
-console.log('chrome extension runrunrun')
-
-var elts = document.getElementsByTagName('p');
-    for (var i = 0; i < elts.length; i++) {
-      elts[i].style['background-color'] = 'purple';
-    }
